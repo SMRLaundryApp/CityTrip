93d7cff2887c1feafa05329eec7e5ab55ba5dc5a
Bart3009 <bartvanveggel@gmail.com>
2020-03-20 16:07:02 +0100

    Can get location from user

--- MapView/App.js
+++ MapView/App.js
@@ -3,16 +3,53 @@ import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions, PermissionsAndroid, Platform } from 'react-native';
 // import Geolocation from '@react-native-community/geolocation';
+import Constants from 'expo-constants';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 
 
 export default class App extends React.Component {
+
+  state = {
+    location: null,
+    errorMessage: null,
+  };
+
+  constructor(props) {
+    super(props);
+    if (Platform.OS === 'android' && !Constants.isDevice) {
+      this.setState({
+        errorMessage: 'Oops, this will not work on Sketch in an Android emulator. Try it on your device!',
+      });
+    } else {
+      this._getLocationAsync();
+    }
+  }
+
+  _getLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      this.setState({
+        errorMessage: 'Permission to access location was denied',
+      });
+    }
+
+    let location = await Location.getCurrentPositionAsync({});
+    this.setState({ location });
+  };
+
+  _getCurrentLocationAsync = async () => {
+
+  };
+
   render() {
-    
+
     // Geolocation.getCurrentPosition(info => console.log(info));
     
     return (
       <View style={styles.container}>
+        <Text>{JSON.stringify(this.state.location)}</Text>
         <MapView
           style={styles.mapStyle}
           initialRegion={{
@@ -38,6 +75,6 @@ const styles = StyleSheet.create({
   },
   mapStyle: {
     width: '100%',
-    height: '100%',
+    height: '80%',
   },
 });
