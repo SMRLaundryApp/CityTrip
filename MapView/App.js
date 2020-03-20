e56ed59b931f1d3ae89fefae92f4bf39e72ab59c
Bart3009 <bartvanveggel@gmail.com>
2020-03-20 16:39:52 +0100

    Can show the latitude and longitude from startup

--- MapView/App.js
+++ MapView/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import MapView from 'react-native-maps';
 import { Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, Dimensions, PermissionsAndroid, Platform } from 'react-native';
@@ -7,12 +7,12 @@ import Constants from 'expo-constants';
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
-
+const GEOLOCATION_OPTIONS = { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 };
 
 export default class App extends React.Component {
 
   state = {
-    location: null,
+    location: { coords: { latitude: null, longitude: null}},
     errorMessage: null,
   };
 
@@ -35,13 +35,40 @@ export default class App extends React.Component {
       });
     }
 
-    let location = await Location.getCurrentPositionAsync({});
-    this.setState({ location });
+    Location.watchPositionAsync(GEOLOCATION_OPTIONS, this.locationChanged);
+
+    // let location = await Location.getCurrentPositionAsync({});
+    // this.setState({ location });
   };
 
-  _getCurrentLocationAsync = async () => {
+  locationChanged = (location) => {
+    // region = {
+    //   latitude: location.coords.latitude,
+    //   longitude: location.coords.longitude,
+    //   latitudeDelta: 0.1,
+    //   longitudeDelta: 0.05,
+    // },
+    this.setState({location/*, region*/})
+  }
 
-  };
+  // _getCurrentLocationAsync = async () => {
+  //   let { status } = await Permissions.askAsync(Permissions.LOCATION);
+  //   if (status !== 'granted') {
+  //     this.setState({
+  //       errorMessage: 'Permission to acces location was denied',
+  //     });
+  //   }
+
+  //   let currentLocation = await Location.watchPositionAsync({
+  //     accuracy: 6,
+  //     timeInterval: 5000,
+  //    },
+  //    (location) => callback(this.setState({location})));
+  // };
+
+  // showLocation(currentLocation) {
+  //   this.setState({ location });
+  // }
 
   render() {
 
@@ -49,7 +76,9 @@ export default class App extends React.Component {
     
     return (
       <View style={styles.container}>
-        <Text>{JSON.stringify(this.state.location)}</Text>
+        <Text>Longitude: {JSON.stringify(this.state.location.coords.longitude)}</Text>
+        <Text>Latitude: {JSON.stringify(this.state.location.coords.latitude)}</Text>
+        <Text>Timestamp: {JSON.stringify(this.state.location.timestamp)}</Text>
         <MapView
           style={styles.mapStyle}
           initialRegion={{
