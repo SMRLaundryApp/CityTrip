de6f2b1596c1905df339b439a11ec4e812caef8e
Bart3009 <bartvanveggel@gmail.com>
2020-03-24 13:01:38 +0100

    Changed App.js structure

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import MapView, { Callout, Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, ScrollView, Dimensions, PermissionsAndroid, Platform, TouchableOpacity } from 'react-native';
 import Constants from 'expo-constants';
@@ -7,10 +7,14 @@ import * as Permissions from 'expo-permissions';
 import styles from './Style';
 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+
+  render() {
+    
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
 }
