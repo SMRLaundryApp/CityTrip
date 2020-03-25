8e701ecd37d688774fa1fdaaea2c0f83dd8a46c5
Bart3009 <bartvanveggel@gmail.com>
2020-03-25 11:52:40 +0100

    Updated the Map screen

--- screens/Home_map.js
+++ screens/Home_map.js
@@ -1,35 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import MapView, { Marker } from 'react-native-maps';
-import Constants from 'expo-constants';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
 import styled from 'styled-components';
 import Welcomebar from '../Components/Welcomebar';
 import Map from '../Components/Map';
 
-
-
-
 export default class Home_map extends React.Component{
-    
-    
-      render(){
-    
-        return (
-          <Container>
-            <Welcomebar />
-            
-            <Map />
-  
-              
-            </Container>
-        );
-      }
-    }
-    
-    const Container = styled.View`
-        flex: 1;
-        background-color: white;
-    `;
-    
+  render(){
+    return (
+      <Container>
+        <Welcomebar />
+        <Map showUserLocation={true}/>
+      </Container>
+    );
+  }
+}
+
+const Container = styled.View`
+  flex: 1;
+  background-color: white;
+`;
