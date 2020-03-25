66ffa10579a1f6c07565fecef0308eba566fc41b
Bart3009 <bartvanveggel@gmail.com>
2020-03-25 10:29:02 +0100

    Updated stuff

--- App.js
+++ App.js
@@ -1,10 +1,6 @@
 import React, { Component } from 'react';
-import MapView, { Callout, Marker } from 'react-native-maps';
 import { StyleSheet, Text, View, ScrollView, Dimensions, PermissionsAndroid, Platform, TouchableOpacity } from 'react-native';
 import styled from 'styled-components';
-import Constants from 'expo-constants';
-import * as Location from 'expo-location';
-import * as Permissions from 'expo-permissions';
 import styles from './Style';
 import Map from './Map';
 
@@ -15,9 +11,7 @@ export default class App extends Component {
 
     return (
       <View style={styles.container}>
-        <Map 
-        showUserLocation={true}
-        />
+        <Map showUserLocation={true} />
       </View>
     );
   }
--- README.md
+++ README.md
@@ -5,5 +5,8 @@
     expo install expo-location    
     expo install expo-constants    
     expo install expo-permissions
-    react-native link
     npm install --save styled-components
+    npm install @react-navigation/native
+    expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
+    npm install @react-navigation/bottom-tabs
+    react-native link
