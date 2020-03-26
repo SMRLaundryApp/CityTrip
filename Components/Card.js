14f8015f9c7f9fa6eb721a7a2c8ebf5980dbb0cf
Bart3009 <bartvanveggel@gmail.com>
2020-03-26 10:12:22 +0100

    Changed the appearance of the cards in the POI_list screen

--- Components/Card.js
+++ Components/Card.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { TouchableOpacity } from 'react-native-gesture-handler';
- 
+import { Dimensions } from 'react-native';
 
+// TODO: Change <Title /> to the location as the crow flies
+
+const cardWidth = Dimensions.get('window').width / 2 - 20;
 
 export default class Card extends Component {
 	
@@ -11,7 +14,7 @@ export default class Card extends Component {
 		let card = [];
 		card.push(
 			<TouchableOpacity  onLongPress={() => console.log('roekoeroekoe')}>
-				<Container>
+				<Container width={cardWidth}>
 					<Cover>
 						<Image source={{uri:this.props.image}} />
 					</Cover>
@@ -37,12 +40,10 @@ const Container = styled.View`
 	align-self: center;
 	background: #888;
 	height: 200px;
-	width: 100%;
 	border-radius: 14px;
 	margin: 5%;
 	margin-top: 20px;
 	box-shadow: 0 5px 15px rgba(0, 0, 0, 0.30); /*This particular line doesn't seem to do anything in android OS*/
-
 `;
 
 const Cover = styled.View`
@@ -51,25 +52,27 @@ const Cover = styled.View`
   border-top-left-radius: 14px;
   border-top-right-radius: 14px;
   overflow: hidden;
-`
+`;
 
 const Image = styled.Image`
   width: 100%;
   height: 100%;
-`
+`;
 
 const Content = styled.View`
   padding-top: 10px;
   flex-direction: column;
   align-items: center;
   height: 60px;
-`
+`;
 
 const Title = styled.Text`
+  padding-left: 5px;
+  padding-right: 5px;
 	color: #FFF;
-	font-size: 16px;
+	font-size: 18px;
 	font-weight: 600;
-	align-items: center;
+	text-align: center;
 `;
 
 const CityName = styled.Text`
