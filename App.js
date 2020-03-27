583df6a2dd5a3fbb444c7139aa5d8120cced4d39
Coleslaw1 <61027282+Coleslaw1@users.noreply.github.com>
2020-03-27 10:58:11 +0100

    added a new theme color

--- App.js
+++ App.js
@@ -60,7 +60,7 @@ export default function App() {
           },
         })}
         tabBarOptions={{
-          activeTintColor: 'seagreen',
+          activeTintColor: '#19B092',
           inactiveTintColor: 'gray',
         }}
       >
