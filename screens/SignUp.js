bbc9c4fc7ada9808cc2132a8c4696469697f1d99
Coleslaw1 <61027282+Coleslaw1@users.noreply.github.com>
2020-03-31 16:59:35 +0200

    finished the signup screen (till further notice)

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -4,20 +4,32 @@ import {
   TouchableHighlight,
   TouchableOpacity,
 } from 'react-native-gesture-handler'
-import { TextInput } from 'react-native'
-import { CheckBox } from 'react-native-elements'
+import { TextInput, Linking } from 'react-native'
+import { CheckBox, Input } from 'react-native-elements'
+
+var state = {
+  checked: false
+}
 
 export default ({ navigation }) => (
+
   <Screen source={require('../assets/test_background.jpg')}>
     <Container>
       <Title>Sign-up</Title>
       <InputTitle>E-mail address:</InputTitle>
       <Inputfield placeholder={'E-mail address'} />
       <InputTitle>Password:</InputTitle>
-      <Inputfield placeholder={'Password'} />
+      <Inputfield placeholder={'Password'} secureTextEntry={true} />
       <InputTitle>Repeat password:</InputTitle>
-      <Inputfield placeholder={'Repeat password'} />
-      <CheckBox center title="I agree with the terms of use" />
+      <Inputfield placeholder={'Repeat password'} secureTextEntry={true}/>
+      <CheckBox 
+      center 
+      title="I agree with the terms of use" 
+      onPress={() => alert("You stupid?!")}
+      />
+      <Butoon>
+        <Butoontext onPress={() => navigation.navigate}>Sign-up</Butoontext>
+      </Butoon>
     </Container>
   </Screen>
 )
@@ -32,30 +44,31 @@ const Container = styled.View`
   width: 90%;
   align-self: center;
   margin-top: 20%;
-  opacity: 0.8;
+  opacity: 0.9;
 `
 
 const Inputfield = styled.TextInput`
   background-color: white;
-  margin-left: 20px;
+  margin-left: 10px;
   margin-bottom: 20px;
-  margin-right: 20px;
+  margin-right: 10px;
   height: 40px;
 `
 
 const Title = styled.Text`
   align-self: center;
   color: #fff;
-  font-size: 40px;
+  font-size: 60px;
   font-weight: 400;
   margin: 20px;
   text-shadow: 2px 2px 4px #000;
+  margin-bottom: 80px;
 `
 
 const InputTitle = styled.Text`
   align-self: auto;
   color: #fff;
-  margin-left: 20px;
+  margin-left: 10px;
   margin-bottom: 5px;
   font-size: 20px;
   font-weight: 400;
@@ -69,10 +82,7 @@ const Butoon = styled.View`
   background-color: #88c868;
   border-radius: 100px;
   margin: 10px;
-`
-
-const Buttoon2 = styled(Butoon)`
-  background-color: #61a93d;
+  margin-top: 20%;
 `
 
 const Butoontext = styled.Text`
